de05f0771ceceb7cc05f3929cb1bc7bc517b5af0
Muskan kumari <kumarikmmuskan12@gmail.com>
2025-01-20 16:00:24 +0530

    hi

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello Muskan!!";
+
+    return 0;
+}
+
+//command to run code
+// Command : "gcc hello.cpp -o hello.exe"  (Afterb this hello.exe file is created)
+// Command : ".\hello.exe"
